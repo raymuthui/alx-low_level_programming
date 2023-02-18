0c4e42900442106853d7bc4eb5d69b9ffe3da10b
raymuthui <muthui816@gmail.com>
2023-02-18 09:54:29 -0800

    trial 1

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main - Entry
+ *
+ * Return: 0(Successful)
+ */
+int main(void)
+{
+        char x;
+
+        x = 'a';
+        while
+                (x <= 'z') {
+                        putchar(x);
+                        x++;
+                }
+        putchar('\n');
+        return (0);
+
+}
---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+/**
+ * main - Entry
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	char x;
+	char y;
+	
+	x = 'a';
+	y = 'A';
+        while
+                (x <= 'z') {
+                        putchar(x);
+                        x++;
+                }
+        while
+                (y <= 'Z') {
+                        putchar(y);
+                        y++;
+                }
+        putchar('\n');
+        return (0);
+}
