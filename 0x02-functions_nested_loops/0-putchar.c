24e2027a3bb5099b009a5b7abf39c857854906a3
raymuthui <muthui816@gmail.com>
2023-02-21 11:52:13 -0800

    fixed betty for putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,19 +1,19 @@
-#include "main.h"
-/*
+#include <stdio.h>
+/**
  * main - entry
  *
- * Return: 0 (success)
+ * Return: 0(success)
  */
 int main(void)
 {
 	_putchar('_');
-        _putchar('p');
-        _putchar('u');
-        _putchar('t');
-        _putchar('c');
-        _putchar('h');
-        _putchar('a');
-        _putchar('r');
-        _putchar('\n');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
 	return (0);
 }
