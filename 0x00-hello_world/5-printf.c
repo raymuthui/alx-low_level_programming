4730e705341a3dbbad2a5e40cbcba64101618eff
raymuthui <muthui816@gnail.com>
2023-07-13 23:43:51 +0300

    Write fixes for 5-printf.c to fit with betty style

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,13 +1,13 @@
 #include <stdio.h>
 
-/*
- * main - entry
+/**
+ * main - Entry point
  *
- * Return: 0
+ * Return: Always 0
  */
 int main(void)
 {
-	printf("with proper grammar, but the outcome is a piece of art,");
+	printf("with proper grammar, but the outcome is a piece of art,\n");
 
 	return (0);
 }
