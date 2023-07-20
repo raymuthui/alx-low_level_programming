2439dad4e8cd23ac8307e4083a58481e5319191d
raymuthui <muthui816@gnail.com>
2023-07-20 14:26:01 +0300

    Create a function that multiplies 2 digits

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * mul - multiply
+ * @a: store first digit
+ * @b: store second digit
+ *
+ * Return: multiplied value
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}
