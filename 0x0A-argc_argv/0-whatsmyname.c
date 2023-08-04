aa9b63345e904e79537627671515573b6ec709cf
raymuthui <muthui816@gnail.com>
2023-08-04 19:05:01 +0300

    Create a program that prints its name using argv

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - entry
+ * @argc: argument count
+ * @argv: argument vector
+ *
+ * Return: name
+ */
+int main (int argc, char *argv[])
+{
+	argc = argc;
+	printf("%s\n", argv[0]);
+	
+	return (0);
+}
