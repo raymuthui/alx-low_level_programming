8eb7a11f8b89f0b33ddfd821d3bc8aa3d9871530
raymuthui <muthui816@gnail.com>
2023-08-08 22:47:53 +0300

    Write a function that returns a pointer to address containing duplicate string

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,23 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * _strdup - function that returns pointer to newly allocated space
+ * @str: string to allocate
+ *
+ * Return: string
+ */
+char *_strdup(char *str)
+{
+	int i, j;
+	char *mst;
+
+	if (str == NULL)
+		return (NULL);
+	while (str[i] != '\0')
+		i++;
+	mst = malloc(i * sizeof(char));
+	for (j = 0; str[j]; j++)
+		mst[j] = str[j];
+	return (mst);
+}
