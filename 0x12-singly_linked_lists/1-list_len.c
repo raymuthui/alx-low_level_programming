bd62f9fac380bda866504dccf42be879401f56b5
michelleresian <michellresian@gmail.com>
2023-08-25 00:35:38 +0300

    return the number of elements in a linked list

---
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -0,0 +1,15 @@
+#include "lists.h"
+/**
+ * list_len - length of the list
+ * @h: list
+ *
+ * Return: list length
+ */
+size_t list_len(const list_t *h)
+{
+	size_t size = 0;
+
+	while (h)
+		size++;
+	return (size);
+}
