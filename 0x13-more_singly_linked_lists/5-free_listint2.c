02ce2d346f96ad13064c427eff846145eaf8ab70
raymuthui <muthui816@gmail.com>
2023-08-30 20:53:35 +0300

    Function that frees a list

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,19 @@
+#include "lists.h"
+/**
+ * free_listint2 - saves memory by freeing a linked list
+ * @head: pointer to list
+ */
+void free_listint2(listint_t **head)
+{
+	listint_t *short;
+
+	if (head == NULL)
+		return;
+	while (*head)
+	{
+		short = (*head)->next;
+		free(*head);
+		*head = short;
+	}
+	*head - NULL;
+}
